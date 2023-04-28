401da4fc9f61e150c1146c420f265e6e33507612
brianreicher <reicher.b@northeastern.edu>
2023-04-28 00:49:16 -0400

    Disrcim updates

--- models.py
+++ models.py
@@ -5,92 +5,106 @@ import torch.nn.functional as F
 # ADAPTED FROM https://github.com/aitorzip/PyTorch-CycleGAN neeed to ResidualBlock, could not develop specific network architecturces myself that created sufficient results
 
 class ResidualBlock(nn.Module):
-    def __init__(self, in_features) -> None:
+    def __init__(self, fmaps) -> None:
+        """
+            Create a new ResidualBlock for our generator. Adapted from PyTorch-CycleGAN.
+        """
         super(ResidualBlock, self).__init__()
 
-        conv_block:list = [  nn.ReflectionPad2d(1),
-                        nn.Conv2d(in_features, in_features, 3),
-                        nn.InstanceNorm2d(in_features),
+        block:list = [  nn.ReflectionPad2d(1),
+                        nn.Conv2d(fmaps, fmaps, 3),
+                        nn.InstanceNorm2d(fmaps),
                         nn.ReLU(inplace=True),
                         nn.ReflectionPad2d(1),
-                        nn.Conv2d(in_features, in_features, 3),
-                        nn.InstanceNorm2d(in_features)  ]
+                        nn.Conv2d(fmaps, fmaps, 3),
+                        nn.InstanceNorm2d(fmaps)  ]
 
-        self.conv_block: nn.Sequential = nn.Sequential(*conv_block)
+        self.conv_block: nn.Sequential = nn.Sequential(*block) # pass the block list into the conv block sequential object 
 
     def forward(self, x):
-        return x + self.conv_block(x)
+        """ 
+            Forward pass to execute model pipeline on data and add it to the existing tensor
+        """
+        return self.conv_block(x) + x
 
 class Generator(nn.Module):
-    def __init__(self, input_nc, output_nc, n_residual_blocks=9) -> None:
+    def __init__(self, input_nc=3, output_nc=3, n_residual_blocks=9) -> None:
+        """
+            Constructor, builds model pipeline
+        """
         super(Generator, self).__init__()
 
-        # Initial convolution block       
+        # conv block for immediate feature extraction       
         model:list = [   nn.ReflectionPad2d(3),
                     nn.Conv2d(input_nc, 64, 7),
                     nn.InstanceNorm2d(64),
-                    nn.ReLU(inplace=True) ]
+                    nn.SELU(inplace=True) ]
 
-        # Downsampling
-        in_features:int = 64
-        out_features:int = in_features*2
+        # execute 2 layers of downsampling to extract 128 feature maps 
+        fmaps:int = 64
+        out_features:int = fmaps*2
         for _ in range(2):
-            model += [  nn.Conv2d(in_features, out_features, 3, stride=2, padding=1),
+            # additional conv block for more features
+            model += [  nn.Conv2d(fmaps, out_features, 3, stride=2, padding=1),
                         nn.InstanceNorm2d(out_features),
-                        nn.ReLU(inplace=True) ]
-            in_features = out_features
-            out_features = in_features*2
+                        nn.SELU(inplace=True) ]
+            fmaps = out_features
+            out_features = fmaps*2
 
-        # Residual blocks
+        # execute residual blocks on the initial number of fmaps
         for _ in range(n_residual_blocks):
-            model += [ResidualBlock(in_features)]
+            model += [ResidualBlock(fmaps)]
 
-        # Upsampling
-        out_features = in_features//2
+        # upsample downsampled data to retrieve initial sizing
+        out_features = fmaps//2 # divide and round down from fmaps in case of odd value
         for _ in range(2):
-            model += [  nn.ConvTranspose2d(in_features, out_features, 3, stride=2, padding=1, output_padding=1),
+            model += [  nn.ConvTranspose2d(fmaps, out_features, 3, stride=2, padding=1, output_padding=1),
                         nn.InstanceNorm2d(out_features),
                         nn.ReLU(inplace=True) ]
-            in_features = out_features
-            out_features = in_features//2
+            fmaps = out_features
+            out_features = fmaps//2
 
-        # Output layer
+        # output with hyperbolic tangent activation function
         model += [  nn.ReflectionPad2d(3),
                     nn.Conv2d(64, output_nc, 7),
                     nn.Tanh() ]
 
-        self.model = nn.Sequential(*model)
+        self.model: nn.Sequential = nn.Sequential(*model)
 
     def forward(self, x):
+        """
+            Forward pass to execute model pipeline on data
+        """
         return self.model(x)
 
 class Discriminator(nn.Module):
-    def __init__(self, input_nc):
+    def __init__(self, input_nc=3) -> None:
+        """
+            Constructor, builds model pipeline.
+        """
         super(Discriminator, self).__init__()
 
-        # repeated convolutional passes to extract features
-        model = [   nn.Conv2d(input_nc, 64, 4, stride=2, padding=1),
-                    nn.LeakyReLU(0.2, inplace=True) ]
+        # repeated convolutional passes to extract features from the initial input channels (3 RGB) through 512 channels for classification
+        # execute normalations after convolution to ensure standardization
+        model:list = [nn.Conv2d(input_nc, 64, 4, stride=2, padding=1),
+                      nn.LeakyReLU(0.2, inplace=True)]
 
-        model += [  nn.Conv2d(64, 128, 4, stride=2, padding=1),
-                    nn.InstanceNorm2d(128), 
-                    nn.LeakyReLU(0.2, inplace=True) ]
+        inc:list = [1, 2, 4]
+        for i in inc:
+            model += [nn.Conv2d(64*i, 128*i, 4, stride=2, padding=1),
+                    nn.InstanceNorm2d(128*i), 
+                    nn.LeakyReLU(0.2, inplace=True)]
 
-        model += [  nn.Conv2d(128, 256, 4, stride=2, padding=1),
-                    nn.InstanceNorm2d(256), 
-                    nn.LeakyReLU(0.2, inplace=True) ]
-
-        model += [  nn.Conv2d(256, 512, 4, padding=1),
-                    nn.InstanceNorm2d(512), 
-                    nn.LeakyReLU(0.2, inplace=True) ]
-
-        # FCN classification layer
+        # conv without activation for classification
         model += [nn.Conv2d(512, 1, 4, padding=1)]
 
         # pass convs to squential model
-        self.model = nn.Sequential(*model)
+        self.model: nn.Sequential = nn.Sequential(*model)
 
     def forward(self, x):
+        """
+            Forward pass to execute model pipeline on data
+        """
         x =  self.model(x)
-        # Average pooling and flatten
+        # execute average pooling and flattening of the tensor
         return F.avg_pool2d(x, x.size()[2:]).view(x.size()[0], -1)
